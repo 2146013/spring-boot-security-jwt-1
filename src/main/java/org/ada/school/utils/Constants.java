aece175e16286b2ed9c1df6bb3a1cc46778b8ae4
Daniela pachon <laura.pachon-c@mail.escuelaing.edu.co>
2021-09-08 18:57:11 -0500

    Modificaciones

---
+++ src/main/java/org/ada/school/utils/Constants.java
@@ -0,0 +1,12 @@
+package org.ada.school.utils;
+
+public class Constants {
+    public static String COOKIE_NAME = "ada-JWT";
+
+    public static  String CLAIMS_ROLES_KEY = "roles";
+
+    public static int TOKEN_DURATION_MINUTES = 1440;
+
+    public static  String ADMIN_ROLE = "ADMIN";
+    public static  String USER_ROLE = "USER";
+}
